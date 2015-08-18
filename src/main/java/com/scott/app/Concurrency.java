d7dfcb8b17b8e1033c94ed94bf656454d53bde9f
scottware <n3iNWtj1>
2015-08-18 14:50:30 -0700

    foo

--- src/main/java/com/scott/app/Concurrency.java
+++ src/main/java/com/scott/app/Concurrency.java
@@ -1,19 +1,18 @@
 package com.scott.app;
 
-public class Concurrency
-{ 
-	public static void main(String [] args) {
+public class Concurrency {
+	public static void main(String[] args) {
 		Counter c = new Counter();
 		Thread a = new Thread(new ThreadTest(c));
 		Thread b = new Thread(new ThreadTest(c));
-	
-	        long startTime = System.currentTimeMillis(); // Get the start Time
+
+		long startTime = System.currentTimeMillis(); // Get the start Time
 
 		a.start();
 		b.start();
 
-	        long endTime = System.currentTimeMillis(); // Get the end Time
+		long endTime = System.currentTimeMillis(); // Get the end Time
 		long delta = endTime - startTime;
-		System.out.println( "time: " + Long.toString( delta ) );
+		System.out.println("time: " + Long.toString(delta));
 	}
 }
